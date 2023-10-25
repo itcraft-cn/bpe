28e1e07bc7d080a6ec2db2d6d9301e66baf740bc
Helly Guo <hellyguo@foxmail.com>
2023-10-25 15:40:17 +0800

    test: sample for profiler

---
+++ shower4j/src/test/java/com/erayt/shower4j/ShowerSample.java
@@ -0,0 +1,67 @@
+package com.erayt.shower4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+/**
+ * @author Helly Guo
+ * <p>
+ * Created on 10/25/23 2:10 PM
+ */
+public class ShowerSample {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShowerSample.class);
+
+    private static final String SQL = "select _1.__1 from _1 limit 1";
+
+    private static final byte[] DATA = {
+            1, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+    };
+
+    private static final int LOOP_SIZE = 10000000;
+
+    public static void main(String[] args) {
+        Shower.start();
+        if (Shower.defActionWithCallback(SQL, ShowerSample::listenData)) {
+            waitCmd();
+        } else {
+            LOGGER.warn("failed to def action");
+        }
+        Shower.stop();
+    }
+
+    private static void listenData(long[] data, int size) {
+    }
+
+    private static void waitCmd() {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        while (true) {
+            LOGGER.info("waiting for input:");
+            line = scanner.nextLine();
+            if ("exit".equals(line)) {
+                break;
+            } else if ("run".equals(line)) {
+                sendData();
+            }
+        }
+        LOGGER.info("ready to quit");
+    }
+
+    private static void sendData() {
+        long start = System.nanoTime();
+        for (int i = 0; i < LOOP_SIZE; i++) {
+            Shower.newData(1, DATA);
+        }
+        long end = System.nanoTime();
+        LOGGER.info("send {} data in {} ms, {} ns", LOOP_SIZE, (end - start) / 1000D / 1000D, end - start);
+    }
+}
