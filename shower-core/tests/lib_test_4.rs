ba6950e08481e62e1f602b36b1260f68b511f588
Helly Guo <hellyguo@foxmail.com>
2023-10-26 09:58:07 +0800

    test: test parse new func

--- shower-core/tests/lib_test_4.rs
+++ shower-core/tests/lib_test_4.rs
@@ -1,16 +1,20 @@
 use shower::{def_action, start, stop};
 use std::env;
 
-const SQL: &str = r#"
-    SELECT _1.__1
-    FROM _1
-    LIMIT 1
-    "#;
-
 #[test]
 fn test_new_proc() {
+    let test_sql_vec: Vec<&str> = vec![
+        r#"SELECT _1.__1 FROM _1 LIMIT 1"#,
+        r#"SELECT _add(_1.__1, _1.__2) FROM _1 LIMIT 1"#,
+        r#"SELECT _sub(_1.__1, _1.__2) FROM _1 LIMIT 1"#,
+        r#"SELECT _mul(_1.__1, _1.__2) FROM _1 LIMIT 1"#,
+        r#"SELECT _div(_1.__1, _1.__2) FROM _1 LIMIT 1"#,
+        r#"SELECT _mod(_1.__1, _1.__2) FROM _1 LIMIT 1"#,
+    ];
     env::set_var("SHOWER_HOME", "/home/helly/code/rust/shower");
     start();
-    def_action(SQL);
+    for sql in test_sql_vec {
+        def_action(sql);
+    }
     stop();
 }
