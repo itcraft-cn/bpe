5fc258d11302a150db5b2f12074860a118342383
Helly Guo <hellyguo@foxmail.com>
2023-11-16 00:17:57 +0800

    feat: can be called by python

--- pyshower/pyshower/shower.py
+++ pyshower/pyshower/shower.py
@@ -34,6 +34,36 @@ class Shower:
             bool: True if stop success. 是否成功
         """
         return shower4py.stop()
+    
+    @staticmethod
+    def def_incoming(name:str, names:list,types:list,lengths:list)->int:
+        """Define incoming var name. 通过 name 定义输入
+
+        Args:
+            name (str): name. 名称
+            names (list): names. 名称列表
+            types (list): types. 类型列表
+            lengths (list): lengths. 长度列表
+
+        Returns:
+            int: id. 标号
+        """
+        return shower4py.def_incoming(name, names, types, lengths)
+    
+    @staticmethod
+    def def_stream(name:str, names:list,types:list,lengths:list)->int:
+        """Define stream var name. 通过 name 定义流
+
+        Args:
+            name (str): name. 名称
+            names (list): names. 名称列表
+            types (list): types. 类型列表
+            lengths (list): lengths. 长度列表
+
+        Returns:
+            int: id. 标号
+        """
+        return shower4py.def_stream(name, names, types, lengths)
 
     @staticmethod
     def new_data(id: int, bdata: bytes) -> bool:
@@ -49,26 +79,40 @@ class Shower:
         return shower4py.new_data(id, bdata)
 
     @staticmethod
-    def def_action(sql:str)-> bool:
-        """Define action var sql. 通过 sql 定义动作
-
-        Args:
-            sql (str): sql statement. sql 语句。
-
-        Returns:
-            bool: True if define success. 是否成功
-        """
-        return shower4py.def_action(sql)
-
-    @staticmethod
-    def def_action_with_callback(sql:str, callback:ShowerRecordCallback)-> bool:
-        """Define action var sql. 通过 sql 定义动作
+    def def_mapper(sql:str, callback:ShowerRecordCallback)-> bool:
+        """Define mapper var sql. 通过 sql 定义动作
 
         Args:
             sql (str): sql statement. sql 语句。
             callback (ShowerRecordCallback): callback function. 回调函数
 
         Returns:
-            bool: True if define success. 是否成功
+            int: >=0 if define success. 是否成功
         """
-        return shower4py.def_action_with_callback(sql, callback)
+        return shower4py.def_mapper(sql, callback)
+
+    @staticmethod
+    def def_mapper_bind_aggregate(sql:str, aggregate_id:int)-> int:
+        """Define mapper var sql. 通过 sql 定义动作
+
+        Args:
+            sql (str): sql statement. sql 语句。
+            callback (ShowerRecordCallback): callback function. 回调函数
+
+        Returns:
+            int: >=0 if define success. 是否成功
+        """
+        return shower4py.def_mapper_bind_aggregate(sql, aggregate_id)
+
+    @staticmethod
+    def def_aggregate(sql:str, callback:ShowerRecordCallback)-> int:
+        """Define aggregate var sql. 通过 sql 定义动作
+
+        Args:
+            sql (str): sql statement. sql 语句。
+            callback (ShowerRecordCallback): callback function. 回调函数
+
+        Returns:
+            int: >=0 if define success. 是否成功
+        """
+        return shower4py.def_aggregate(sql, callback)
--- pyshower/tests/test_shower2.py
+++ pyshower/tests/test_shower2.py
@@ -12,6 +12,7 @@ from pyshower.shower import Shower, ShowerRecordCallback
 import pytest
 
 SQL = "select demo.a from demo limit 10"
+SQL2 = "select _suml(stream.a) from stream"
 
 
 class XDemo(ShowerRecordCallback):
@@ -36,9 +37,12 @@ class TestUniStream:
     def test(self):
         callback = XDemo()
         Shower.def_incoming("demo", ["a"], [0], [0])
+        Shower.def_stream("stream", ["a"], [0], [0])
         global SQL
-        mapper_id = Shower.def_mapper(SQL, callback)
-        print("mapper_id:", mapper_id)
+        global SQL2
+        aggregate_id = Shower.def_aggregate(SQL2, callback)
+        mapper_id = Shower.def_mapper_bind_aggregate(SQL, aggregate_id)
+        print("mapper_id:", mapper_id, "aggregate_id:", aggregate_id)
         for i in range(100):
             Shower.new_data(1, b"100000000000000000000000")
 
