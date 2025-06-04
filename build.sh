6a4e747b2140c8fd621f3a369c36ce5e80c4421c
Helly Guo <hellyguo@foxmail.com>
2025-06-04 17:12:45 +0800

    build: just one shell to build

--- build.sh
+++ build.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo without param, as default, simple build, debug
+    CHOOSE=0
+    elif [ $# -eq 1 ]; then
+    CHOOSE=$1
+else
+    echo param error, quit
+    exit 255
+fi
+
+if [ $CHOOSE -eq 0 ]; then
+    echo simple build, debug
+    CARGO_CMD="cargo build"
+    FULL_BUILD=0
+elif [ $CHOOSE -eq 1 ]; then
+    echo simple build, release
+    CARGO_CMD="cargo build --release"
+    FULL_BUILD=0
+elif [ $CHOOSE -eq 2 ]; then
+    echo full build, debug
+    CARGO_CMD="cargo build"
+    MATURIN_CMD="maturin build"
+    FULL_BUILD=1
+elif [ $CHOOSE -eq 3 ]; then
+    echo full build, release
+    CARGO_CMD="cargo build --release"
+    MATURIN_CMD="maturin build --release"
+    FULL_BUILD=1
+else
+    echo param error, quit
+    exit 255
+fi
+
 rm -rf target/wheels/*.whl
 
 # Build core
-#cargo build
-RUSTFLAGS='-Zub-checks=no' cargo build     
+RUSTFLAGS='-lLLVM-14 -Zub-checks=no' $CARGO_CMD
 
-# Build java
-cd bambootube4j
-mvnd package -Dmaven.test.skip=true
-cd ..
-
-# Build python
-cd bambootube-py-wrapper
-source bin/activate
-#maturin build
-RUSTFLAGS='-Zub-checks=no' maturin build     
-deactivate
-cd ..
-
-# Build python wrapper
-#whl_file_name=$(ls target/wheels/bambootube4py-*)
-#cd pybambootube
-#source bin/activate
-#pip install ../$whl_file_name --force-reinstall
-#deactivate
-#cd ..
-
+if [ $FULL_BUILD -eq 1 ]; then
+    # Build java
+    cd bambootube4j
+    mvnd package -Dmaven.test.skip=true
+    cd ..
+    
+    # Build python wrapper
+    cd bambootube-py-wrapper
+    source bin/activate
+    RUSTFLAGS='-lLLVM-14 -Zub-checks=no' $MATURIN_CMD
+    deactivate
+    cd ..
+    
+    # install to python sample
+    whl_file_name=$(ls target/wheels/bambootube4py-*)
+    cd pysample
+    source bin/activate
+    pip install ../$whl_file_name --force-reinstall
+    deactivate
+    cd
+fi
--- simple_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build core
-#RUSTFLAGS='-lLLVM-14' cargo build --release
-RUSTFLAGS='-lLLVM-14 -Zub-checks=no' cargo build
-#RUSTFLAGS='-Zub-checks=no' cargo build
-#cargo build
-
