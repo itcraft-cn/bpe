2aa76d313d11b3087ea248245193247299a77afe
Helly Guo <hellyguo@foxmail.com>
2025-06-04 18:05:32 +0800

    build: use different params to build

--- build.sh
+++ build.sh
@@ -14,20 +14,24 @@ if [ $CHOOSE -eq 0 ]; then
     echo simple build, debug
     CARGO_CMD="cargo build"
     FULL_BUILD=0
+    RUSTFLAGS='-lLLVM-14 -Zub-checks=no'
 elif [ $CHOOSE -eq 1 ]; then
     echo simple build, release
     CARGO_CMD="cargo build --release"
     FULL_BUILD=0
+    RUSTFLAGS='-lLLVM-14'
 elif [ $CHOOSE -eq 2 ]; then
     echo full build, debug
     CARGO_CMD="cargo build"
     MATURIN_CMD="maturin build"
     FULL_BUILD=1
+    RUSTFLAGS='-lLLVM-14 -Zub-checks=no'
 elif [ $CHOOSE -eq 3 ]; then
     echo full build, release
     CARGO_CMD="cargo build --release"
     MATURIN_CMD="maturin build --release"
     FULL_BUILD=1
+    RUSTFLAGS='-lLLVM-14'
 else
     echo param error, quit
     exit 255
@@ -36,7 +40,7 @@ fi
 rm -rf target/wheels/*.whl
 
 # Build core
-RUSTFLAGS='-lLLVM-14 -Zub-checks=no' $CARGO_CMD
+RUSTFLAGS=$RUSTFLAGS $CARGO_CMD
 
 if [ $FULL_BUILD -eq 1 ]; then
     # Build java
@@ -47,7 +51,7 @@ if [ $FULL_BUILD -eq 1 ]; then
     # Build python wrapper
     cd bambootube-py-wrapper
     source bin/activate
-    RUSTFLAGS='-lLLVM-14 -Zub-checks=no' $MATURIN_CMD
+    RUSTFLAGS=$RUSTFLAGS $MATURIN_CMD
     deactivate
     cd ..
     
