c0cb5db3afa78717e9b2b77688f235f7e6b8a69d
Helly Guo <hellyguo@foxmail.com>
2026-01-08 10:05:23 +0800

    refactor: jit log

--- bpe-core/src/jit/log.rs
+++ bpe-core/src/jit/log.rs
@@ -1,78 +1,65 @@
 use crate::{cfg::get_config, consts::KEY_DEV_MODE, jit::base::GenContext};
-use inkwell::values::{FloatValue, IntValue};
+use inkwell::values::{BasicValue, BasicValueEnum};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Once,
 };
 
-/// Conditionally logs a value from JIT-compiled code if development mode is enabled.
-/// This function checks the configuration to determine if logging should be performed.
-pub(crate) fn _call_log_int<'ctx>(context: &GenContext<'ctx>, val: IntValue<'_>, msg: &str) {
-    static DEV: AtomicBool = AtomicBool::new(false); // Static flag for development mode
-    static STOP: Once = Once::new(); // Once for one-time initialization
-    STOP.call_once(|| {
+static _DEV: AtomicBool = AtomicBool::new(false); // Static flag for development mode
+static _FETCH_DEV_MODE: Once = Once::new(); // Once for one-time initialization
+
+fn _init() {
+    _FETCH_DEV_MODE.call_once(|| {
         // Initialize the DEV flag by reading the configuration once
-        DEV.store(get_config().fetch_cfg_bool(KEY_DEV_MODE), Ordering::SeqCst);
+        _DEV.store(get_config().fetch_cfg_bool(KEY_DEV_MODE), Ordering::SeqCst);
     });
-    if DEV.load(Ordering::SeqCst) {
-        // Only call the actual log function if development mode is enabled
-        _actual_call_log_int(context, val, msg);
-    }
 }
 
 /// Conditionally logs a value from JIT-compiled code if development mode is enabled.
 /// This function checks the configuration to determine if logging should be performed.
-pub(crate) fn _call_log_float<'ctx>(context: &GenContext<'ctx>, val: FloatValue<'_>, msg: &str) {
-    static DEV: AtomicBool = AtomicBool::new(false); // Static flag for development mode
-    static STOP: Once = Once::new(); // Once for one-time initialization
-    STOP.call_once(|| {
-        // Initialize the DEV flag by reading the configuration once
-        DEV.store(get_config().fetch_cfg_bool(KEY_DEV_MODE), Ordering::SeqCst);
-    });
-    if DEV.load(Ordering::SeqCst) {
+pub(crate) fn _call_log<'ctx>(context: &GenContext<'ctx>, val: &dyn BasicValue<'_>, msg: &str) {
+    _init();
+    if _DEV.load(Ordering::SeqCst) {
         // Only call the actual log function if development mode is enabled
-        _actual_call_log_float(context, val, msg);
+        _actual_call_log(context, val, msg);
     }
 }
 
 /// Actually performs the call to the logging function in the JIT environment.
 /// Builds an LLVM call instruction to invoke the logging function with the specified parameters.
-fn _actual_call_log_int<'ctx>(context: &GenContext<'ctx>, val: IntValue<'_>, msg: &str) {
-    let log_func = context._log_int_func; // Get the logging function from context
+fn _actual_call_log<'ctx>(context: &GenContext<'ctx>, val: &dyn BasicValue<'_>, msg: &str) {
     let desc_msg_ptr = msg.as_ptr(); // Get pointer to the message string
     let i64_type = context.func_generator.context.i64_type(); // Get i64 type for LLVM
-                                                              // Create LLVM constant for the message pointer
-    let desc_msg_ptr_val = i64_type.const_int(desc_msg_ptr as u64, false);
-    // Create LLVM constant for the message length
-    let desc_len = i64_type.const_int(msg.len() as u64, false);
-    let _ = context
-        .func_generator
-        .builder
-        .build_call(
-            log_func,                                                // The logging function to call
-            &[val.into(), desc_msg_ptr_val.into(), desc_len.into()], // Parameters: value, message pointer, length
-            "log", // Name for the call instruction
-        )
-        .unwrap();
-}
 
-/// Actually performs the call to the logging function in the JIT environment.
-/// Builds an LLVM call instruction to invoke the logging function with the specified parameters.
-fn _actual_call_log_float<'ctx>(context: &GenContext<'ctx>, val: FloatValue<'_>, msg: &str) {
-    let log_func = context._log_float_func; // Get the logging function from context
-    let desc_msg_ptr = msg.as_ptr(); // Get pointer to the message string
-    let i64_type = context.func_generator.context.i64_type(); // Get i64 type for LLVM
-                                                              // Create LLVM constant for the message pointer
+    // Create LLVM constant for the message pointer
     let desc_msg_ptr_val = i64_type.const_int(desc_msg_ptr as u64, false);
     // Create LLVM constant for the message length
     let desc_len = i64_type.const_int(msg.len() as u64, false);
-    let _ = context
-        .func_generator
-        .builder
-        .build_call(
-            log_func,                                                // The logging function to call
-            &[val.into(), desc_msg_ptr_val.into(), desc_len.into()], // Parameters: value, message pointer, length
-            "log", // Name for the call instruction
-        )
-        .unwrap();
+
+    let val_enum = val.as_basic_value_enum();
+    let rs = match val_enum {
+        BasicValueEnum::IntValue(v) => Ok((
+            context._log_int_func, // Get the logging function from context
+            v.into(),              // Parameter: value
+        )),
+        BasicValueEnum::FloatValue(v) => Ok((
+            context._log_float_func, // Get the logging function from context
+            v.into(),                // Parameter: value
+        )),
+        _ => {
+            log::warn!("Invalid value type for logging: {val_enum:?}");
+            Err("Invalid value type for logging")
+        }
+    };
+    if let Ok((func, val_meta)) = rs {
+        let _ = context
+            .func_generator
+            .builder
+            .build_call(
+                func,                                                  // The logging function to call
+                &[val_meta, desc_msg_ptr_val.into(), desc_len.into()], // Parameters: value, message pointer, length
+                "log", // Name for the call instruction
+            )
+            .unwrap();
+    }
 }
