76478696d3cbc326a6502cfe2997fcf63ac110c0
Helly Guo <hellyguo@foxmail.com>
2026-09-25 21:22:01 +0800

    chore: remove java/py wrapper

--- bpe-py-wrapper/python/pybpe/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for pybpe."""
-
-__author__ = """Guo Zheyong"""
-__email__ = 'guozheyong@erayt.com'
-__version__ = '0.1.0'
--- bpe4j/src/main/java/cn/itcraft/bpe4j/BpeCallback.java
+++
@@ -1,5 +0,0 @@
-package cn.itcraft.bpe4j;
-
-public interface BpeCallback {
-    void callback(byte[] data, int size);
-}
